52e8c689bc068d4fd2a03729b026ea1d4d5f65cc
Pavel Litvinko <99415318+Neolit1979@users.noreply.github.com>
2022-06-28 15:27:25 +0200

    init: writing function Reverse
    init: writing function Reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    if (n<0) {
+        n=-n;
+    }
+    const str = n.toString();
+    let res = '';
+    for (let i = str.length-1; i>=0; i-- ){
+       res = res+ str[i];
+    } 
   
+    return Number(res)
 }
